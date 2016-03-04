406dc8379aae1ef2a77e3b8623c5bdfda778a3f8
Nick Dunn <nick.dunn@wearefriday.com>
2016-03-04 18:26:49 +0000

    Use Request.execute so a timeout can be specified

--- lib/spectre_client.rb
+++ lib/spectre_client.rb
@@ -5,23 +5,34 @@ module SpectreClient
   class Client
     def initialize(project_name, suite_name, url_base)
       @url_base = url_base
-      request = RestClient.post("#{@url_base}/runs",
-        project: project_name,
-        suite: suite_name
+      request = RestClient::Request.execute(
+        method: :post,
+        url: "#{@url_base}/runs",
+        timeout: 120,
+        payload: {
+          project: project_name,
+          suite: suite_name
+        }
       )
       response = JSON.parse(request.to_str)
       @run_id = response['id']
     end
 
     def submit_test(name, browser, platform, width, screenshot)
-      request = RestClient.post("#{@url_base}/tests",
-        test: {
-          run_id: @run_id,
-          name: name,
-          platform: platform,
-          browser: browser,
-          width: width,
-          screenshot: screenshot
+      request = RestClient::Request.execute(
+        method: :post,
+        url: "#{@url_base}/tests",
+        timeout: 120,
+        :multipart: true,
+        payload: {
+          test: {
+            run_id: @run_id,
+            name: name,
+            platform: platform,
+            browser: browser,
+            width: width,
+            screenshot: screenshot
+          }
         }
       )
       JSON.parse(request.to_str, symbolize_names:true)
